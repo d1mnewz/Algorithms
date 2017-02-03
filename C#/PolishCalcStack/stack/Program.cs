d72447b3246b7f493dfc3311c40cc76dd283db55
ZhluktenkoDmytro <d1mnewz@gmail.com>
2017-02-03 18:49:12 +0200

    added reverse polish notation calculator implemented using stack via C#

---
+++ C#/PolishCalcStack/stack/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+namespace StackRPN
+{
+   
+    class Program
+    {
+        public static void Main()
+        {
+            RPNCalc obj = new RPNCalc();
+           
+            while (true)
+            {
+                obj.ReadInput();
+                if (obj.GetCount() == 0) // case of empty string
+                {
+                    continue;
+                }
+                try
+                {
+                    double result = obj.EvaluateResult();
+                    if (obj.GetCount() != 0) 
+                    {
+                        throw new Exception();
+                    }
+                    Console.WriteLine(result);
+                }
+                catch (Exception) { Console.WriteLine("error"); }
+            }
+        }
+    }
+}
---
+++ C#/PolishCalcStack/stack/RPNCalc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackRPN
+{
+    public class RPNCalc
+    {
+        private readonly char[] _separators = new char[] { ' ', '\t' };
+        private Stack<string> _tokenizedStack = new Stack<string>();
+        private void AssignStack(string input)
+        {
+            this._tokenizedStack = new Stack<string>
+                      (input.Split(this._separators, StringSplitOptions.RemoveEmptyEntries));
+        }
+
+        public double EvaluateResult()
+        {
+            string token = this._tokenizedStack.Pop();
+            double x, y;
+            if (!Double.TryParse(token, out x))
+            {
+                y = EvaluateResult(); x = EvaluateResult();
+                if (token == "+") x += y;
+                else if (token == "-") x -= y;
+                else if (token == "*") x *= y;
+                else if (token == "/") x /= y;
+                else throw new Exception();
+            }
+            return x;
+        }
+        public int GetCount()
+        {
+            return _tokenizedStack.Count;
+        }
+        public void ReadInput()
+        {
+            AssignStack(Console.ReadLine());
+        }
+    }
+}
