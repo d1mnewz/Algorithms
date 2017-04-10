04e31fd9f30113d5700af41c956d4674dddd1949
ZhluktenkoDmytro <d1mnewz@gmail.com>
2017-04-10 04:11:24 +0300

    Update InsertionSort.fsx

--- F#/Sortings/InsertionSort.fsx
+++ F#/Sortings/InsertionSort.fsx
@@ -1,12 +1,11 @@
-//https://cdn.programiz.com/sites/tutorial2program/files/Insertion-Sort-Algorithm-Programming.jpg
-//https://www.tutorialspoint.com/data_structures_algorithms/insertion_sort_algorithm.htm
-//Step 1 − If it is the first element, it is already sorted. return 1;
-//Step 2 − Pick next element
-//Step 3 − Compare with all elements in the sorted sub-list
-//Step 4 − Shift all the elements in the sorted sub-list 
+
+// If it is the first element, it is already sorted. return 1;
+// Pick next element
+// Compare with all elements in the sorted sub-list
+// Shift all the elements in the sorted sub-list 
 //          that is greater than the value to be sorted
-//Step 5 − Insert the value
-//Step 6 − Repeat until list is sorted
+// Insert the value
+// Repeat until list is sorted
 let mutable arr = [|1;9;4;2;8;3;6;4;3;2;4;5;6;7;7;7|];
  
 let insert (arr: 'a[]) i item =  // where i = index
